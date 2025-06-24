593d3bdd5cec6e831290a450da9db7ff2fc3b2b3
ColinJimmy <colinjimmy2004@gmail.com>
2025-06-24 15:40:07 +0530

    chat functionality updated

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,8 +1,9 @@
-class Constants {
-  static String appId="1:375773056324:web:77b201d894c9d9189b5b11";
-  static String apiKey="AIzaSyC6ZF77JUMlkhfbhjAp8iU5SiqrwdOJLiQ";
-  static String messagingSenderId="375773056324";
-  static String projectId="chatapp-flutter-6d00f";
+import 'package:flutter/material.dart';
 
-  get primaryColor => null;
+class Constants {
+  static String appId = "1:634480644405:web:5549258ea987afaefb7c7c";
+  static String apiKey = "AIzaSyADZ6Uq-pFQIWMdkGeh7JIozEY7jJM6r_I";
+  static String messagingSenderId = "634480644405";
+  static String projectId = "chatappflutter-13318";
+  final primaryColor = const Color(0xFFee7b64);
 }
