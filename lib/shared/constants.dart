45d5a3b3dc4eb8ea5edefe6a6fd9e2ab2f0a2593
ColinJimmy <colinjimmy2004@gmail.com>
2025-06-24 14:27:16 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static String appId="1:375773056324:web:77b201d894c9d9189b5b11";
+  static String apiKey="AIzaSyC6ZF77JUMlkhfbhjAp8iU5SiqrwdOJLiQ";
+  static String messagingSenderId="375773056324";
+  static String projectId="chatapp-flutter-6d00f";
+
+  get primaryColor => null;
+}
